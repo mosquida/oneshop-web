2e384bbc8a45ab7f42b2875fb5a98c4a86a3afae
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-24 11:52:07 +0800

    Created home and products-list component @ shop

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomeComponent,
+    ProductsListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
