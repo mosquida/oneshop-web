16bcf6b485644d8049d019f47dbe858574651524
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-26 12:12:48 +0800

    Added array of valid component selector prefix at eslintrc

--- apps/shop/src/app/app.component.ts
+++ apps/shop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'shop-web-root',
+  selector: 'oneshop-web-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
