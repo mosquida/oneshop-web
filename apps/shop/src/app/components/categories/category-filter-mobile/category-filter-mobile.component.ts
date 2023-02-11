43a06ed3ee9cfceb8659fa8ab801bbdc2b7007d7
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2023-02-11 14:00:35 +0800

    Show Dynamic Category Filter

--- apps/shop/src/app/components/categories/category-filter-mobile/category-filter-mobile.component.ts
+++ apps/shop/src/app/components/categories/category-filter-mobile/category-filter-mobile.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Category } from '@oneshop-web/categories';
 
 @Component({
   selector: 'oneshop-web-category-filter-mobile',
   templateUrl: './category-filter-mobile.component.html',
   styleUrls: ['./category-filter-mobile.component.scss'],
 })
-export class CategoryFilterMobileComponent {}
+export class CategoryFilterMobileComponent implements OnInit {
+  categories: Category[] = [];
+
+  constructor(private categoriesService: CategoriesService) {}
+
+  ngOnInit() {
+    this.categoriesService
+      .getCategories()
+      .subscribe((categories) => (this.categories = categories));
+  }
+}
--- apps/shop/src/app/components/categories/category-filter/category-filter.component.ts
+++ apps/shop/src/app/components/categories/category-filter/category-filter.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Category } from '@oneshop-web/categories';
 
 @Component({
   selector: 'oneshop-web-category-filter',
   templateUrl: './category-filter.component.html',
   styleUrls: ['./category-filter.component.scss'],
 })
-export class CategoryFilterComponent {}
+export class CategoryFilterComponent implements OnInit {
+  categories: Category[] = [];
+
+  constructor(private categoriesService: CategoriesService) {}
+
+  ngOnInit() {
+    this.categoriesService
+      .getCategories()
+      .subscribe((categories) => (this.categories = categories));
+  }
+
+  categoryFilter(id: string) {
+    alert(id);
+  }
+}
