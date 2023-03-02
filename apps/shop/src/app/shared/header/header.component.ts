44ca1a557bbb1137b60f632c7bbc64a734577912
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2023-03-02 21:39:14 +0800

    Added logout on Shop

--- apps/shop/src/app/shared/header/header.component.ts
+++ apps/shop/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@oneshop-web/auth';
 import { CartService } from '@oneshop-web/orders';
 
 @Component({
@@ -9,12 +10,24 @@ import { CartService } from '@oneshop-web/orders';
 export class HeaderComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   cartCount: string = '0';
+  openAccount = true;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((cart) => {
       this.cartCount = cart.items.length.toString();
     });
+  }
+
+  triggerOpenAccount(): void {
+    this.openAccount = !this.openAccount;
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
