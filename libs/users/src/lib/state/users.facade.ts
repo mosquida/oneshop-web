5f66d95f0636eff92cb8c83ae5e60bc79fceaae7
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2023-04-02 00:09:15 +0800

    NgRX State lib setup with facade for User State store

---
+++ libs/users/src/lib/state/users.facade.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { select, Store, Action } from '@ngrx/store';
+
+import * as UsersActions from './users.actions';
+import * as UsersFeature from './users.reducer';
+import * as UsersSelectors from './users.selectors';
+
+@Injectable()
+export class UsersFacade {
+  private readonly store = inject(Store);
+
+  /**
+   * Combine pieces of state using createSelector,
+   * and expose them as observables through the facade.
+   */
+  loaded$ = this.store.pipe(select(UsersSelectors.getUsersLoaded));
+  allUsers$ = this.store.pipe(select(UsersSelectors.getAllUsers));
+  selectedUsers$ = this.store.pipe(select(UsersSelectors.getSelected));
+
+  /**
+   * Use the initialization action to perform one
+   * or more tasks in your Effects.
+   */
+  init() {
+    this.store.dispatch(UsersActions.initUsers());
+  }
+}
